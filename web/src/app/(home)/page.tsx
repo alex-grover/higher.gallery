625851f962790b503f50b20de7ff9cd918059af5
↑ <higher-aligned@proton.me>
2024-05-24 17:38:09 -0400

    Add new page and global main styles

--- web/src/app/(home)/page.tsx
+++ web/src/app/(home)/page.tsx
@@ -3,12 +3,11 @@
 import { Masonry } from 'masonic'
 import { data } from './data'
 import { Nft } from './nft'
-import styles from './page.module.css'
 
 export default function HomePage() {
   return (
-    <main className={styles.main}>
-      <Masonry items={data} render={Nft} columnWidth={280} columnGutter={8} />
+    <main>
+      <Masonry items={data} render={Nft} columnWidth={240} columnGutter={8} />
     </main>
   )
 }
