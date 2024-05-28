ef6db46297f5aec95b35e43128fb08c327a29e31
↑ <higher-aligned@proton.me>
2024-05-28 18:41:37 -0400

    Add token page layout

--- web/src/app/(token)/[address]/[id]/page.tsx
+++ web/src/app/(token)/[address]/[id]/page.tsx
@@ -1,3 +1,19 @@
+import styles from './page.module.css'
+
 export default function TokenPage() {
-  return <main>TODO: token page</main>
+  return (
+    <main className={styles.container}>
+      <div className={styles.image}>TODO: image</div>
+      <div className={styles.details}>
+        <div className={styles.metadata}>
+          <div className={styles.name}>TODO: name</div>
+          <div className={styles.artist}>TODO: artist</div>
+          <div className={styles.collection}>TODO: collection</div>
+        </div>
+        <button className={styles.button}>Mint for TODO: price</button>
+        <div>TODO: time remaining, mint count, supply limit</div>
+        <div>TODO: comment feed</div>
+      </div>
+    </main>
+  )
 }
