8e0d4794e5d99d32c01fca20549b05c37dee4bb0
↑ <higher-aligned@proton.me>
2024-05-30 18:58:40 -0400

    build: add deploy script

---
+++ protocol/script/Deploy.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {Script} from "forge-std/Script.sol";
+import {Higher1155} from "src/Higher1155.sol";
+import {Higher1155Factory} from "src/Higher1155Factory.sol";
+
+contract DeployScript is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        Higher1155 implementation = new Higher1155();
+        Higher1155Factory factory = new Higher1155Factory(address(implementation));
+
+        vm.stopBroadcast();
+    }
+}
