ac0d49595b281c423cf6b00aefc0a501bbc57642
↑ <higher-aligned@proton.me>
2024-05-31 02:52:02 -0400

    chore: add todos for create page

--- apps/web/src/app/new/page.tsx
+++ apps/web/src/app/new/page.tsx
@@ -1,3 +1,13 @@
 export default function CreatePage() {
-  return <main>TODO</main>
+  return (
+    <main>
+      <div>TODO: asset</div>
+      <div>
+        <div>TODO: collection</div>
+        <div>TODO: name, description</div>
+        <div>TODO: price</div>
+        <div>TODO: time, supply limit</div>
+      </div>
+    </main>
+  )
 }
